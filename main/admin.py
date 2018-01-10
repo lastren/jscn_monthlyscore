84cccbef4a14ed9fb05d274ab30bbae50c719ad3
sorge <yaozhipeng@aliyun.com>
2018-01-10 10:22:43 +0800

    report add to admin界面

--- main/admin.py
+++ main/admin.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Report
 
 # Register your models here.
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ('id','status','month','author',)
+    search_fields = ('author',)
+    list_filter = ['author']
+
+
+admin.site.register(Report,ReportAdmin)
